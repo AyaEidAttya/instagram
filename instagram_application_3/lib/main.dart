597b97bdbf2e71f5e0b64dac099b0c3957680fa7
aya eid <ayaaeid.72@gmail.com>
2023-04-14 21:23:09 +0200

    run responsivee

--- instagram_application_3/lib/main.dart
+++ instagram_application_3/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_application_3/responsive/mobile.dart';
+import 'package:instagram_application_3/responsive/responsive.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-   home: mobilescreen(),
+   home:responsivee(),
 
 
     );
