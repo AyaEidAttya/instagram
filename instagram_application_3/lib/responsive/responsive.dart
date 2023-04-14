c94c9f0d2f4bcf764632150944a26acb1fa1eeec
aya eid <ayaaeid.72@gmail.com>
2023-04-14 21:31:14 +0200

    layoutbulder

--- instagram_application_3/lib/responsive/responsive.dart
+++ instagram_application_3/lib/responsive/responsive.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
 class responsivee extends StatefulWidget {
+  final mymobile_screen;
+  final myweb_screen;
 
-final mymobile_screen;
-final myweb_screen;
-
-
-
-  const responsivee({super.key,required this.mymobile_screen,required this.myweb_screen});
+  const responsivee(
+      {super.key, required this.mymobile_screen, required this.myweb_screen});
 
   @override
   State<responsivee> createState() => _responsiveeState();
@@ -17,18 +15,11 @@ class _responsiveeState extends State<responsivee> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (buildContext, boxConstraints) {
-        if (boxConstraints.maxWidth > 600) {
-         return
-
-
-        },
-    else 
-    {
-      return
-    }
-
-
-    }
-);
+      if (boxConstraints.maxWidth > 600) {
+        return widget.myweb_screen;
+      } else {
+        return widget.mymobile_screen;
+      }
+    });
   }
 }
