dc8b27d20bca2892e81468cfa68d4d9787fffb7a
aya eid <ayaaeid.72@gmail.com>
2023-04-14 21:19:55 +0200

    add layoutbuilder

---
+++ instagram_application_3/lib/responsive/responsive.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class responsivee extends StatefulWidget {
+  const responsivee({super.key});
+
+  @override
+  State<responsivee> createState() => _responsiveeState();
+}
+
+class _responsiveeState extends State<responsivee> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (buildContext, boxConstraints) {}
+);
+  }
+}
