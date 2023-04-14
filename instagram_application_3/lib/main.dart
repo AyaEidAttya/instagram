5442a12405f8633d86f879d70d635dec8d11f174
aya eid <ayaaeid.72@gmail.com>
2023-04-14 21:13:29 +0200

    run mobilescreen

--- instagram_application_3/lib/main.dart
+++ instagram_application_3/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_application_3/responsive/mobile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-   home: ,
-
+   home: mobilescreen(),
 
 
     );
