98d42d710f8e45c46e86869de322d4b6becc56cf
aya eid <ayaaeid.72@gmail.com>
2023-04-14 21:05:45 +0200

    add class mobile screen

--- instagram_application_3/lib/responsive/mobile.dart
+++ instagram_application_3/lib/responsive/mobile.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class mobilescreen extends StatefulWidget {
+  const mobilescreen({super.key});
+
+  @override
+  State<mobilescreen> createState() => _mobilescreenState();
+}
+
+class _mobilescreenState extends State<mobilescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
