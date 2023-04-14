21e92f0d4f7154b7d7c5819203b1bbfc2e467756
aya eid <ayaaeid.72@gmail.com>
2023-04-14 21:10:41 +0200

    appbar mobile screen

--- instagram_application_3/lib/responsive/mobile.dart
+++ instagram_application_3/lib/responsive/mobile.dart
@@ -10,6 +10,10 @@ class mobilescreen extends StatefulWidget {
 class _mobilescreenState extends State<mobilescreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("mobile screen"),
+      ),
+    );
   }
 }
