2b7c6dc9700c99e122418ba75992863c826f616e
aya eid <ayaaeid.72@gmail.com>
2023-04-14 21:26:03 +0200

    add tow var

--- instagram_application_3/lib/responsive/responsive.dart
+++ instagram_application_3/lib/responsive/responsive.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class responsivee extends StatefulWidget {
-  const responsivee({super.key});
+
+final mymobile_screen;
+final myweb_screen;
+
+
+
+  const responsivee({super.key,required this.mymobile_screen,required this.myweb_screen});
 
   @override
   State<responsivee> createState() => _responsiveeState();
@@ -10,7 +16,19 @@ class responsivee extends StatefulWidget {
 class _responsiveeState extends State<responsivee> {
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (buildContext, boxConstraints) {}
+    return LayoutBuilder(builder: (buildContext, boxConstraints) {
+        if (boxConstraints.maxWidth > 600) {
+         return
+
+
+        },
+    else 
+    {
+      return
+    }
+
+
+    }
 );
   }
 }
